aea30d3e7a7ac4ba4418f711d5f4b0a7345ab833
andymc <andymc@udel.edu>
2015-09-01 21:48:34 +0000

    changed hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main(){
-    std::cout << "What a world this is!" << std::endl;
+    std::cout << "What a world this is!\n Which is worse, ignorance or apathy? \n I don't know and I don't care!" << std::endl;
 }
